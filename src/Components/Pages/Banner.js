8b3a5d31949a7186a4b1d3a2e56a466713810ada
omarfaruk7022 <farhanfaruk221@gmail.com>
2022-11-19 09:51:03 +0600

    start

--- src/Components/Pages/Banner.js
+++ src/Components/Pages/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from "react-typical";
 
 const Banner = () => {
   return (
@@ -14,12 +15,22 @@ const Banner = () => {
 
         <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
           <div class="max-w-xl text-center sm:text-left">
-            <h1 class="text-3xl font-extrabold sm:text-5xl lg:text-white text-black">
-              Let us find your
-              <strong class="block font-extrabold text-accent">
+            <h1 class="text-3xl font-extrabold sm:text-5xl lg:text-accent text-black">
+            <Typical
+                steps={[
+                  "",
+                  1000,
+                  "Welcome to Idol Ace",
+                  500,
+                ]}
+                loop={Infinity}
+                wrapper="p"
+              />
+            </h1>
+             <h1 className="text-3xl font-extrabold sm:text-5xl lg:text-white text-black"> Let us find your</h1>
+              <strong class="text-3xl font-extrabold sm:text-5xl lg:text-neutral text-black">
                 Forever Home.
               </strong>
-            </h1>
 
             <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed lg:text-white text-black">
             Idol Ace Ltd is a International Trading House. Idol Ace Ltd provide all kinds of Equipment. As like as, Cleaning Equipment, Bathroom Equipment, Kitchen Equipment etc. 
